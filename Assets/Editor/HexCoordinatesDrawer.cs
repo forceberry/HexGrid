7a3965e8c797f4cd37795c7793b7168ec72a3fd9
forceberry <otto.forsberg@gmail.com>
2019-03-01 16:00:46 +0200

    Initial commit, pushing everything now when everything functions properly.

---
+++ Assets/Editor/HexCoordinatesDrawer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+[CustomPropertyDrawer(typeof(HexCoordinates))]
+public class HexCoordinatesDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        HexCoordinates coordinates = new HexCoordinates(property.FindPropertyRelative("x").intValue, property.FindPropertyRelative("z").intValue);
+        GUI.Label(position, coordinates.ToString());
+    }
+}
